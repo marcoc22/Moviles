0a2d7f0831847e3c86a2f0c2489a68734460d31e
marcoc22 <mtcanalesmesen@gmail.com>
2017-04-13 15:58:21 -0600

    Aplicación en blanco

---
+++ PlaceFinder506/app/src/main/java/com/una/app/placefinder506/Actividad01.java
@@ -0,0 +1,13 @@
+package com.una.app.placefinder506;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Actividad01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad01);
+    }
+}
