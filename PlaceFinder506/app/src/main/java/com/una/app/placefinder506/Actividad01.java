a8db2cc83bd6eb6574bc68a3ce4ec90dc705107c
Massi20 <massimoro029@gmail.com>
2017-04-13 20:25:26 -0600

    Massiel
    Login (Básico)
    Algunas Categorías (Básico)
    Actividad para agregar la info nuestra

--- PlaceFinder506/app/src/main/java/com/una/app/placefinder506/Actividad01.java
+++ PlaceFinder506/app/src/main/java/com/una/app/placefinder506/Actividad01.java
@@ -1,7 +1,10 @@
 package com.una.app.placefinder506;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Actividad01 extends AppCompatActivity {
 
@@ -9,5 +12,45 @@ public class Actividad01 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad01);
-    }
-}
+
+        // A continuación mi código para OnCreate
+        Mensaje("Bienvenido Actividad 01");
+
+        OnclickDelButton(R.id.btnLogin);
+        OnclickDelButton(R.id.btnApp);
+
+    } // Fin del Oncreate de la Actividad 01
+
+    public void Mensaje(String msg){getSupportActionBar().setTitle(msg);};
+
+    public void OnclickDelButton(int ref) {
+
+        // Ejemplo  OnclickDelButton(R.id.MiButton);
+        // 1 Doy referencia al Button
+        View view =findViewById(ref);
+        Button miButton = (Button) view;
+        //  final String msg = miButton.getText().toString();
+        // 2.  Programar el evento onclick
+        miButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // if(msg.equals("Texto")){Mensaje("Texto en el botón ");};
+                switch (v.getId()) {
+
+                    case R.id.btnLogin:
+                        Intent intento = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(intento);
+
+                        break;
+
+                    case R.id.btnApp:
+                        intento = new Intent(getApplicationContext(), categorias.class);
+                        startActivity(intento);
+
+                        break;
+                    default:break; }// fin de casos
+            }// fin del onclick
+        });
+    }// fin de OnclickDelButton
+
+} // [18:25:04] Fin de la Clase Actividad 01
---
+++ PlaceFinder506/app/src/main/java/com/una/app/placefinder506/DeveloperInfo.java
@@ -0,0 +1,20 @@
+package com.una.app.placefinder506;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeveloperInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer_info);
+        // A continuación mi código para OnCreate
+        Mensaje("Bienvenido DeveloperInfo");
+
+
+    } // Fin del Oncreate de la DeveloperInfo
+
+    public void Mensaje(String msg){getSupportActionBar().setTitle(msg);};
+
+} // [20:01:33] Fin de la Clase DeveloperInfo
