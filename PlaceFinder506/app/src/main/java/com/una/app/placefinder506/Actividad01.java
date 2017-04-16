3352800089bd55a98511d826caddf475305fef7a
marcoc22 <mtcanalesmesen@gmail.com>
2017-04-16 01:07:47 -0600

    Login con google+
    Seguir al minuto 8:20 del video

--- PlaceFinder506/app/src/main/java/com/una/app/placefinder506/Actividad01.java
+++ PlaceFinder506/app/src/main/java/com/una/app/placefinder506/Actividad01.java
@@ -1,13 +1,24 @@
 package com.una.app.placefinder506;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class Actividad01 extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class Actividad01 extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+    private GoogleApiClient googleApiClient;
+    private SignInButton signInButton;
+    public static final int SIGN_IN_CODE = 777;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,11 +26,51 @@ public class Actividad01 extends AppCompatActivity {
 
         // A continuación mi código para OnCreate
         Mensaje("PLACEFINDER506");
-
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+        signInButton = (SignInButton)findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,SIGN_IN_CODE);
+            }
+        });
         OnclickDelButton(R.id.btnLogin);
         OnclickDelButton(R.id.btnApp);
 
     } // Fin del Oncreate de la Actividad 01
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult){
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN_CODE){
+            GoogleSignInResult resultado = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(resultado);
+        }
+    }
+    private void handleSignInResult(GoogleSignInResult resultado){
+        if(resultado.isSuccess()){
+            goMainScreen();
+        }else{
+            Toast.makeText(this,"No se pudo iniciar sesión", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void goMainScreen(){
+        Intent intent = new Intent(this,categorias.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 
     public void Mensaje(String msg){getSupportActionBar().setTitle(msg);};
 
