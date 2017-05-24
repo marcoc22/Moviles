5e7f4482bd2c32a65383b69c766b3e5b714d48f0
Massi20 <massimoro029@gmail.com>
2017-05-23 21:41:32 -0600

    Tenga Paz Bryhan
    Intento fallido de ubicación actual

--- PlaceFinder506/app/src/main/java/com/una/app/placefinder506/FragmentoMapa.java
+++ PlaceFinder506/app/src/main/java/com/una/app/placefinder506/FragmentoMapa.java
@@ -1,33 +1,54 @@
 package com.una.app.placefinder506;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
 
 /**
- * Created by Marco on 18/4/2017.
+ * Created by Marco, Massiel,Angélica,Bryhan on 18/4/2017.
  */
 public class FragmentoMapa extends Fragment implements OnMapReadyCallback {
     private GoogleMap miMapa;
     private Location location;
+    private Marker marcador;
+    double latitud = 0.0;
+    double longitud = 0.0;
+    Context aux2;
+    public Context getAux2() {
+        return aux2;
+    }
+
+    public void setAux2(Context aux2) {
+        this.aux2 = aux2;
+    }
+
+
 
     public Location getLocation() {
         return location;
     }
+
     public void setLocation(Location location) {
         this.location = location;
     }
@@ -43,6 +64,8 @@ public class FragmentoMapa extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         miMapa = googleMap;
+        //si pongo este método se cae
+        //miUbicacion();
         ubicacionActual();
     }
     public void ubicacionActual(){
@@ -51,6 +74,44 @@ public class FragmentoMapa extends Fragment implements OnMapReadyCallback {
         int zoomLevel = 12;
         miMapa.moveCamera(CameraUpdateFactory.newLatLngZoom(milugar, zoomLevel));
     }
+
+    public void addMarcador(double lat, double lng) {
+        LatLng milugar = new LatLng(lat, lng);
+        CameraUpdate miubicacion = CameraUpdateFactory.newLatLngZoom(milugar, 16);
+        if (marcador != null) marcador.remove();
+        marcador = miMapa.addMarker(new MarkerOptions().position(milugar).title("Mi posisición Actual"));
+        miMapa.animateCamera(miubicacion);
+    }
+
+    private void actualizarUbicacion(Location location) {
+        if (location != null) {
+            latitud = location.getLatitude();
+            longitud = location.getLongitude();
+            addMarcador(latitud, longitud);
+        }
+    }
+
+    LocationListener locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarUbicacion(location);
+        }
+    };
+
+    private void miUbicacion() {
+        //aux2.getApplicationContext() esto debería ser this
+        if (ActivityCompat.checkSelfPermission(aux2.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(aux2.getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationManager locationManager = (LocationManager) aux2.getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,15000,0, (android.location.LocationListener) locListener);
+    }
+
+
+
+
     public void ubicarFarmacias(List<Lugar> lugares){
         for(int i=0;i<lugares.size();i++){
             Lugar lugar = lugares.get(i);
